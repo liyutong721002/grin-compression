8c8405f97a9df2fb0d2eccca0fff50b8dd6923e7
liyutong721002 <liyutong721002@gmail.com>
2025-04-28 10:10:26 -0500

    Finish HuffmanTree and Grin

--- src/main/java/edu/grinnell/csc207/compression/Grin.java
+++ src/main/java/edu/grinnell/csc207/compression/Grin.java
@@ -1,49 +1,97 @@
 package edu.grinnell.csc207.compression;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
 
 /**
  * The driver for the Grin compression program.
  */
 public class Grin {
+
     /**
      * Decodes the .grin file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
+     *
      * @param infile the file to decode
      * @param outfile the file to ouptut to
      */
-    public static void decode (String infile, String outfile) {
-        // TODO: fill me in!
+    public static void decode(String infile, String outfile) throws IOException {
+        BitInputStream in = new BitInputStream(infile);
+        BitOutputStream out = new BitOutputStream(outfile);
+        if (in.readBits(32) != 0x736) {
+            throw new IllegalArgumentException();
+        }
+        HuffmanTree tree = new HuffmanTree(in);
+        tree.decode(in, out);
+        in.close();
+        out.close();
     }
 
     /**
-     * Creates a mapping from 8-bit sequences to number-of-occurrences of
-     * those sequences in the given file. To do this, read the file using a
+     * Creates a mapping from 8-bit sequences to number-of-occurrences of those
+     * sequences in the given file. To do this, read the file using a
      * BitInputStream, consuming 8 bits at a time.
+     *
      * @param file the file to read
      * @return a freqency map for the given file
      */
-    public static Map<Short, Integer> createFrequencyMap (String file) {
-        // TODO: fill me in!
-        return null;
+    public static Map<Short, Integer> createFrequencyMap(String file) throws IOException {
+        BitInputStream in = new BitInputStream(file);
+        Map<Short, Integer> freqs = new HashMap<>();
+        while (true) {
+            int value = in.readBits(8);
+            if (value == -1) {
+                break;
+            }
+            short val = (short) value;
+            if (freqs.containsKey(val)) {
+                freqs.put(val, freqs.get(val) + 1);
+            } else {
+                freqs.put(val, 1);
+            }
+        }
+        return freqs;
     }
 
     /**
      * Encodes the given file denoted by infile and writes the output to the
      * .grin file denoted by outfile.
+     *
      * @param infile the file to encode.
      * @param outfile the file to write the output to.
      */
-    public static void encode(String infile, String outfile) {
-        // TODO: fill me in!
+    public static void encode(String infile, String outfile) throws IOException {
+        BitInputStream in = new BitInputStream(infile);
+        BitOutputStream out = new BitOutputStream(outfile);
+        HuffmanTree tree = new HuffmanTree(createFrequencyMap(infile));
+        out.writeBits(0x736, 32);
+        tree.serialize(out);
+        tree.encode(in, out);
+        in.close();
+        out.close();
     }
 
     /**
      * The entry point to the program.
+     *
      * @param args the command-line arguments.
      */
-    public static void main(String[] args) {
-        // TODO: fill me in!
-        System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 3) {
+            System.out.println("Usage: java Grin <encode|decode> <infile> <outfile>");
+            return;
+        }
+        String command = args[0];
+        String infile = args[1];
+        String outfile = args[2];
+        if (command.equals("encode")) {
+            encode(infile, outfile);
+        } else if (command.equals("decode")) {
+            decode(infile, outfile);
+        } else {
+            System.out.println("First argument must be 'encode' or 'decode'");
+            return;
+        }
     }
 }
--- src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
+++ src/main/java/edu/grinnell/csc207/compression/HuffmanTree.java
@@ -1,66 +1,194 @@
 package edu.grinnell.csc207.compression;
 
 import java.util.Map;
+import java.util.*;
 
 /**
  * A HuffmanTree derives a space-efficient coding of a collection of byte
  * values.
  *
- * The huffman tree encodes values in the range 0--255 which would normally
- * take 8 bits.  However, we also need to encode a special EOF character to
- * denote the end of a .grin file.  Thus, we need 9 bits to store each
- * byte value.  This is fine for file writing (modulo the need to write in
- * byte chunks to the file), but Java does not have a 9-bit data type.
- * Instead, we use the next larger primitive integral type, short, to store
- * our byte values.
+ * The huffman tree encodes values in the range 0--255 which would normally take
+ * 8 bits. However, we also need to encode a special EOF character to denote the
+ * end of a .grin file. Thus, we need 9 bits to store each byte value. This is
+ * fine for file writing (modulo the need to write in byte chunks to the file),
+ * but Java does not have a 9-bit data type. Instead, we use the next larger
+ * primitive integral type, short, to store our byte values.
  */
 public class HuffmanTree {
 
+    private static class Node implements Comparable<Node>{
+
+        private int frequency;
+        private short value;
+        private Node left;
+        private Node right;
+
+        Node(Node left, Node right) {
+            this.value = -1;
+            this.frequency = left.frequency + right.frequency;
+            this.left = left;
+            this.right = right;
+        }
+
+        Node(short value, int frequency) {
+            this.value = value;
+            this.frequency = frequency;
+            this.left = null;
+            this.right = null;
+        }
+
+        public int compareTo(Node other) {
+            return this.frequency - other.frequency;
+        }
+    }
+
+    private Node root;
+
     /**
      * Constructs a new HuffmanTree from a frequency map.
+     *
      * @param freqs a map from 9-bit values to frequencies.
      */
-    public HuffmanTree (Map<Short, Integer> freqs) {
-        // TODO: fill me in!
+    public HuffmanTree(Map<Short, Integer> freqs) {
+        freqs.put((short) 256, 1);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (short key : freqs.keySet()) {
+            Node n = new Node(key, freqs.get(key));
+            pq.add(n);
+        }
+        while (pq.size() > 1) {
+            Node left = pq.poll();
+            Node right = pq.poll();
+            Node parent = new Node(left, right);
+            pq.add(parent);
+        }
+        root = pq.poll();
     }
 
     /**
      * Constructs a new HuffmanTree from the given file.
+     *
      * @param in the input file (as a BitInputStream)
      */
-    public HuffmanTree (BitInputStream in) {
-        // TODO: fill me in!
+    public HuffmanTree(BitInputStream in) {
+        root = readh(in);
+    }
+
+    private Node readh(BitInputStream in) {
+        int current = in.readBit();
+        if (current == -1) {
+            return null;
+        }
+        if (current == 1) {
+            Node left = readh(in);
+            Node right = readh(in);
+            return new Node(left, right);
+        } else {
+            short value = (short) in.readBits(9);
+            return new Node(value, 1);
+        }
     }
 
     /**
      * Writes this HuffmanTree to the given file as a stream of bits in a
      * serialized format.
+     *
      * @param out the output file as a BitOutputStream
      */
-    public void serialize (BitOutputStream out) {
-        // TODO: fill me in!
+    public void serialize(BitOutputStream out) {
+        serializeh(root, out);
+
     }
-   
-    /**
-     * Encodes the file given as a stream of bits into a compressed format
-     * using this Huffman tree. The encoded values are written, bit-by-bit
-     * to the given BitOuputStream.
-     * @param in the file to compress.
-     * @param out the file to write the compressed output to.
-     */
-    public void encode (BitInputStream in, BitOutputStream out) {
-        // TODO: fill me in!
+
+    private void serializeh(Node cur, BitOutputStream out) {
+        if (cur.left == null && cur.right == null) {
+            out.writeBit(0);
+            out.writeBits(cur.value, 9);
+        } else {
+            out.writeBit(1);
+            serializeh(cur.left, out);
+            serializeh(cur.right, out);
+        }
     }
 
     /**
-     * Decodes a stream of huffman codes from a file given as a stream of
-     * bits into their uncompressed form, saving the results to the given
-     * output stream. Note that the EOF character is not written to out
-     * because it is not a valid 8-bit chunk (it is 9 bits).
+     * Encodes the file given as a stream of bits into a compressed format using
+     * this Huffman tree. The encoded values are written, bit-by-bit to the
+     * given BitOuputStream.
+     *
+     * @param in the file to compress.
+     * @param out the file to write the compressed output to.
+     */
+    public void encode(BitInputStream in, BitOutputStream out) {
+        Node current = root;
+        Map<Short, String> codes = new HashMap<>();
+        encodeh(root, "", codes);
+        System.out.println("Huffman Codes: " + codes);
+        while (true) {
+            int value = in.readBits(8);
+            if (value == -1) {
+                break;
+            }
+            short val = (short) value;
+            String code = codes.get(val);
+            for (int i = 0; i < code.length(); i++) {
+                int bit = (int) code.charAt(i) - 48;
+                out.writeBit(bit);
+            }
+
+        }
+        String eof = codes.get((short) 256);
+        for (int i = 0; i < eof.length(); i++) {
+            int bit = (int) eof.charAt(i) - 48;
+            out.writeBit(bit);
+        }
+    }
+
+    private void encodeh(Node cur, String curCode, Map<Short, String> codes) {
+        if (cur == null) {
+            return;
+        }
+        if (cur.left == null && cur.right == null) {
+            codes.put(cur.value, curCode);
+            System.out.println("Leaf node: " + cur.value + " has code: " + curCode);
+            return;
+        }
+        System.out.println("Recursing left for node: " + cur.value + " with code: " + curCode + "0");
+        encodeh(cur.left, curCode + "0", codes);
+        System.out.println("Recursing right for node: " + cur.value + " with code: " + curCode + "1");
+        encodeh(cur.right, curCode + "1", codes);
+    }
+
+    /**
+     * Decodes a stream of huffman codes from a file given as a stream of bits
+     * into their uncompressed form, saving the results to the given output
+     * stream. Note that the EOF character is not written to out because it is
+     * not a valid 8-bit chunk (it is 9 bits).
+     *
      * @param in the file to decompress.
      * @param out the file to write the decompressed output to.
      */
-    public void decode (BitInputStream in, BitOutputStream out) {
-        // TODO: fill me in!
+    public void decode(BitInputStream in, BitOutputStream out) {
+        Node current = root;
+        while (true) {
+            int bit = in.readBit();
+            if (bit == -1) {
+                break;
+            }
+            if (bit == 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            
+            if (current.left == null && current.right == null) {
+                if (current.value == 256) {
+                    out.close();
+                    break;
+                }
+                out.writeBits(current.value, 8);
+                current = root;
+            }
+        }
     }
 }
